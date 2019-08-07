f38b3876ffb11820d9f686b9352bc5322d4d74f1
miniyoung37 <miniyoung37@gmail.com>
2019-08-08 03:34:04 +0900

    movie_app step6 propTypes방법

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 
 // api 이용할 경우
 const foodILike = [
-    {   
-        id:1,
+    {
+        id: 1,
         name: "Kimchi",
-        image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+        image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+        rating: 5
     }, {
-        id:2,
+        id: 2,
         name: "Samgyeopsal",
         image: "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSf" +
-                "HS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+                "HS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+        rating: 4.8
     }, {
-        id:3,
+        id: 3,
         name: "Bibimbap",
         image: "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_120" +
-                "0x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+                "0x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+        rating: 4.7
     }, {
-        id:4,
+        id: 4,
         name: "Doncasu",
-        image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+        image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+        rating: 4.5
     }, {
-        id:5,
+        id: 5,
         name: "Kimbap",
         image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512" +
-                "295.jpg"
+                "295.jpg",
+        rating: 4.4
     }
 ];
 /*
@@ -36,22 +42,31 @@ function renderFood(dish){
 전과 같은방법 이지만 새로운 함수 생성
 */
 
-function Food({ name, picture }){
+function Food({ name, picture, rating }){
   return (
   <div>
     <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
     <img src={picture} alt={name} />
   </div>
   );
 }
 
-
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number // isRequired 필수적으로 판단
+};
 
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food 
+        key={dish.id} 
+        name={dish.name} 
+        picture={dish.image} 
+        rating={dish.rating} />
       ))}
     </div>
   );
