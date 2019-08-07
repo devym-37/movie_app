8a1d260de9c793ce34b0c3f12d9b69ffbc7d965b
miniyoung37 <miniyoung37@gmail.com>
2019-08-08 02:29:08 +0900

    movie_app step4 반복 및 이미지 삽입

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from 'react';
 
-function Food({ fav }){
-  return <h4>I like { fav }</h4>;
+
+function Food({ name, picture }){
+  return (
+  <div>
+    <h2>I like {name}</h2>
+    <img src={picture}  />
+  </div>
+  );
 }
+
+// api 이용할 경우
+const foodILike = [
+    {
+        name: "Kimchi",
+        image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+    }, {
+        name: "Samgyeopsal",
+        image: "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSf" +
+                "HS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+    }, {
+        name: "Bibimbap",
+        image: "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_120" +
+                "0x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+    }, {
+        name: "Doncasu",
+        image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    }, {
+        name: "Kimbap",
+        image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512" +
+                "295.jpg"
+    }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="rice" />
-      <Food fav="hambuger" />
-      <Food fav="ramen" />
+      {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
